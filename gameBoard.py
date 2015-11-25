0c577f6ee4701db6636bcb3ebc93e247d99a01c0
fabien townsend <fabientownsend@gmail.com>
2015-11-25 22:49:32 +0100

    feat (board): set mark on the board

--- gameBoard.py
+++ gameBoard.py
@@ -1,10 +1,22 @@
 class GameBoard():
     def __init__(self):
         self.board = self.createBoard()
+        self.currentMark = 'x'
 
     def createBoard(self):
         NB_SPOTS = 9
-        return list("-"*NB_SPOTS)
+        return list('-'*NB_SPOTS)
 
     def getBoard(self):
         return self.board
+
+    def setMark(self, position):
+        if self.board[position] != '-':
+            raise SpotNotEmpty
+        else:
+            self.board[position] = self.currentMark
+
+class SpotNotEmpty(Exception):
+    def __init__(self):
+        self.msg = 'The sport must be free'
+
--- gameBoardTest.py
+++ gameBoardTest.py
@@ -1,5 +1,7 @@
 import unittest
+
 from gameBoard import GameBoard
+from gameBoard import SpotNotEmpty
 
 class TestGameBoard(unittest.TestCase):
     def setUp(self):
@@ -7,14 +9,29 @@ class TestGameBoard(unittest.TestCase):
 
     def testCreateBoardTest(self):
         response = self.gameBoard.createBoard()
-        expected = list("-"*9)
+        expected = list('-'*9)
         self.assertEqual(response, expected)
 
     def testGetBoard_whenNewObject(self):
         self.gameBoard = GameBoard()
         response = self.gameBoard.getBoard()
-        expected = list("-"*9)
+        expected = list('-'*9)
         self.assertEqual(response, expected)
+
+    def testSetMark_whenSpotIsEmpty(self):
+        self.gameBoard.setMark(0)
+        response = self.gameBoard.getBoard()
+        expected = ['x', '-', '-', '-', '-', '-', '-', '-', '-']
+        self.assertEqual(response, expected)
+
+    def testSetMark_whenSpotNotFree(self):
+        self.gameBoard.setMark(0)
+        with self.assertRaises(SpotNotEmpty):
+            self.gameBoard.setMark(0)
+
+    def testSetMark_whenOutOfBoard(self):
+        with self.assertRaises(IndexError):
+            self.gameBoard.setMark(10)
 
 if __name__ == '__main__':
     unittest.main()
