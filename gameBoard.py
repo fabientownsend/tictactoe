d13a6cd1749bf8cff1f63f4c951f03c0697142a6
fabien townsend <fabientownsend@gmail.com>
2015-11-27 00:26:57 +0100

    feat (board): add check if win and reset board methods

--- gameBoard.py
+++ gameBoard.py
@@ -3,6 +3,9 @@ class GameBoard():
         self.board = self.createBoard()
         self.currentMark = 'x'
 
+    def resetBoard(self):
+        self.board = self.createBoard()
+
     def createBoard(self):
         NB_SPOTS = 9
         return list('-'*NB_SPOTS)
@@ -15,6 +18,50 @@ class GameBoard():
             raise SpotNotEmpty
         else:
             self.board[position] = self.currentMark
+
+    def checkLines(self):
+        if (self.board[0] == self.currentMark and
+              self.board[1] == self.currentMark and
+              self.board[2] == self.currentMark):
+            return True
+        if (self.board[3] == self.currentMark and
+              self.board[4] == self.currentMark and
+              self.board[5] == self.currentMark):
+            return True
+        if (self.board[6] == self.currentMark and
+              self.board[7] == self.currentMark and
+              self.board[8] == self.currentMark):
+            return True
+        else:
+            return False
+
+    def checkColumns(self):
+        if (self.board[0] == self.currentMark and
+              self.board[3] == self.currentMark and
+              self.board[6] == self.currentMark):
+            return True
+        if (self.board[1] == self.currentMark and
+              self.board[4] == self.currentMark and
+              self.board[7] == self.currentMark):
+            return True
+        if (self.board[2] == self.currentMark and
+              self.board[5] == self.currentMark and
+              self.board[8] == self.currentMark):
+            return True
+        else:
+            return False
+
+    def checkDiagonals(self):
+        if (self.board[0] == self.currentMark and
+              self.board[4] == self.currentMark and
+              self.board[8] == self.currentMark):
+            return True
+        if (self.board[2] == self.currentMark and
+              self.board[4] == self.currentMark and
+              self.board[6] == self.currentMark):
+            return True
+        else:
+            return False
 
 class SpotNotEmpty(Exception):
     def __init__(self):
--- gameBoardTest.py
+++ gameBoardTest.py
@@ -12,6 +12,19 @@ class TestGameBoard(unittest.TestCase):
         expected = list('-'*9)
         self.assertEqual(response, expected)
 
+    def testResetBoard_whenBoardEmpty(self):
+        self.gameBoard.setMark(5)
+        self.gameBoard.resetBoard()
+        response = self.gameBoard.getBoard()
+        expected = list('-'*9)
+        self.assertEqual(response, expected)
+
+    def testResetBoard_whenBoardNotEmpty(self):
+        self.gameBoard.resetBoard()
+        response = self.gameBoard.getBoard()
+        expected = list('-'*9)
+        self.assertEqual(response, expected)
+
     def testGetBoard_whenNewObject(self):
         self.gameBoard = GameBoard()
         response = self.gameBoard.getBoard()
@@ -32,6 +45,66 @@ class TestGameBoard(unittest.TestCase):
     def testSetMark_whenOutOfBoard(self):
         with self.assertRaises(IndexError):
             self.gameBoard.setMark(10)
+
+    def testCheckLines_whenLineOneWin(self):
+        self.gameBoard.setMark(0)
+        self.gameBoard.setMark(1)
+        self.gameBoard.setMark(2)
+        self.assertEqual(self.gameBoard.checkLines(), True)
+
+    def testCheckLines_whenLineTwoWin(self):
+        self.gameBoard.setMark(3)
+        self.gameBoard.setMark(4)
+        self.gameBoard.setMark(5)
+        self.assertEqual(self.gameBoard.checkLines(), True)
+
+    def testCheckLines_whenLineThreeWin(self):
+        self.gameBoard.setMark(6)
+        self.gameBoard.setMark(7)
+        self.gameBoard.setMark(8)
+        self.assertEqual(self.gameBoard.checkLines(), True)
+
+    def testCheckLines_whenEmpty(self):
+        self.gameBoard.resetBoard()
+        self.assertEqual(self.gameBoard.checkLines(), False)
+
+    def testCheckColumns_whenColumnsOneWin(self):
+        self.gameBoard.setMark(0)
+        self.gameBoard.setMark(3)
+        self.gameBoard.setMark(6)
+        self.assertEqual(self.gameBoard.checkColumns(), True)
+
+    def testCheckColumns_whenColumnsOneWin(self):
+        self.gameBoard.setMark(1)
+        self.gameBoard.setMark(4)
+        self.gameBoard.setMark(7)
+        self.assertEqual(self.gameBoard.checkColumns(), True)
+
+    def testCheckColumns_whenColumnsOneWin(self):
+        self.gameBoard.setMark(2)
+        self.gameBoard.setMark(5)
+        self.gameBoard.setMark(8)
+        self.assertEqual(self.gameBoard.checkColumns(), True)
+
+    def testCheckColumns_whenEmpty(self):
+        self.gameBoard.resetBoard()
+        self.assertEqual(self.gameBoard.checkColumns(), False)
+
+    def testCheckDiagonals_whenColumnsOneWin(self):
+        self.gameBoard.setMark(0)
+        self.gameBoard.setMark(4)
+        self.gameBoard.setMark(8)
+        self.assertEqual(self.gameBoard.checkDiagonals(), True)
+
+    def testCheckDiagonals_whenColumnsOneWin(self):
+        self.gameBoard.setMark(2)
+        self.gameBoard.setMark(4)
+        self.gameBoard.setMark(6)
+        self.assertEqual(self.gameBoard.checkDiagonals(), True)
+
+    def testCheckDiagonals_whenEmpty(self):
+        self.gameBoard.resetBoard()
+        self.assertEqual(self.gameBoard.checkDiagonals(), False)
 
 if __name__ == '__main__':
     unittest.main()
