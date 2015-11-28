b77c052cdcf19962a6677f530318877b96d33b40
fabien townsend <fabientownsend@gmail.com>
2015-11-28 16:50:25 +0100

    feat (ui): console ui display board

---
+++ consoleUI.py
@@ -0,0 +1,9 @@
+class ConsoleUI():
+    def displayBoard(self, board):
+        print("\n")
+        print " %s | %s | %s " %(board[0], board[1], board[2])
+        print("-----------")
+        print " %s | %s | %s " %(board[3], board[4], board[5])
+        print("-----------")
+        print " %s | %s | %s " %(board[6], board[7], board[8])
+        print("\n")
