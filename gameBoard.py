b2256b352eb8253956ecb0ed075fec6cbdb2ddb2
fabien townsend <fabientownsend@gmail.com>
2016-01-19 00:05:00 +0100

    refactor (gameBoard and gamePolicy): change the data structure from array to 2d array

--- gameBoard.py
+++ gameBoard.py
@@ -2,24 +2,29 @@ from marksEnum import Marks
 
 
 class GameBoard():
-    def __init__(self):
+    def __init__(self, boardSize):
+        self.boardSize = boardSize
         self.board = self.createBoard()
 
     def createBoard(self):
-        return [
-            Marks.empty, Marks.empty, Marks.empty,
-            Marks.empty, Marks.empty, Marks.empty,
-            Marks.empty, Marks.empty, Marks.empty
-        ]
+        return [[Marks.empty]*self.boardSize for n in range(self.boardSize)]
 
     def getBoard(self):
         return self.board
 
     def setMark(self, position, mark):
-        if self.board[position] != Marks.empty:
+        row =  position/self.boardSize
+        column = position - row*self.boardSize
+
+        if self.board[row][column] != Marks.empty:
             raise SpotNotEmpty
         else:
-            self.board[position] = mark
+            self.board[row][column] = mark
+
+    def getMark(self, position):
+        row =  position/self.boardSize
+        column = position - row*self.boardSize
+        return self.board[row][column]
 
 class SpotNotEmpty(Exception):
     def __init__(self):
--- gameBoardTest.py
+++ gameBoardTest.py
@@ -7,42 +7,39 @@ from marksEnum import Marks
 
 class TestGameBoard(unittest.TestCase):
     def setUp(self):
-        self.gameBoard = GameBoard()
+        self.boardSize = 3
+        self.gameBoard = GameBoard(self.boardSize)
+        self.newGameBoard = self.gameBoard.createBoard()
 
-    def getEmptyBoard(self):
-        return [
-            Marks.empty, Marks.empty, Marks.empty,
-            Marks.empty, Marks.empty, Marks.empty,
-            Marks.empty, Marks.empty, Marks.empty
-        ]
+    def getEmptyBoard(self, boardSize):
+        return [[Marks.empty]*boardSize for n in range(boardSize)]
 
-    def testCreateBoardTest(self):
-        response = self.gameBoard.createBoard()
-        expected = self.getEmptyBoard()
-        self.assertEqual(response, expected)
+    def testCreateBoardTest_whenBoardThreeByThree(self):
+        emptyGameBoard = self.getEmptyBoard(self.boardSize)
+        self.assertEqual(self.newGameBoard, emptyGameBoard)
 
-    def testGetBoard_whenNewBoardCreated(self):
-        self.gameBoard = GameBoard()
-        response = self.gameBoard.getBoard()
-        expected = self.getEmptyBoard()
-        self.assertEqual(response, expected)
+    def testCreateBoardTest_whenBoardFourByFour(self):
+        boardSize = 4
+        gameBoard = GameBoard(boardSize)
+        newGameBoard = gameBoard.createBoard()
+        emptyGameBoard = self.getEmptyBoard(boardSize)
+        self.assertEqual(newGameBoard, emptyGameBoard)
 
     def testSetMark_whenSpotIsEmpty(self):
-        self.gameBoard.setMark(0, Marks.cross)
-        response = self.gameBoard.getBoard()
-        expected = [
-            Marks.cross, Marks.empty, Marks.empty,
-            Marks.empty, Marks.empty, Marks.empty,
-            Marks.empty, Marks.empty, Marks.empty
-        ]
-        self.assertEqual(response, expected)
+        mark = Marks.cross
+
+        self.gameBoard.setMark(0, mark)
+        markSetOnBoard = self.gameBoard.getMark(0)
+        self.assertEqual(mark, markSetOnBoard)
 
     def testSetMark_whenSpotNotFree(self):
+        mark = Marks.cross
         self.gameBoard.setMark(0, Marks.cross)
         with self.assertRaises(SpotNotEmpty):
             self.gameBoard.setMark(0, Marks.cross)
 
     def testSetMark_whenOutOfBoard(self):
+        mark = Marks.cross
         with self.assertRaises(IndexError):
             self.gameBoard.setMark(10, Marks.cross)
 
